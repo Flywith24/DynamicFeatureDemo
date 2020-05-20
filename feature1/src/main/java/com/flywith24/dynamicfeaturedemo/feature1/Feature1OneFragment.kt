421a7794b39be483a46994c770a8fdd17a076d50
fly_with24 <fly_with24@163.com>
2020-05-20 17:53:04 +0800

    rename layout

--- feature1/src/main/java/com/flywith24/dynamicfeaturedemo/feature1/Feature1OneFragment.kt
+++ feature1/src/main/java/com/flywith24/dynamicfeaturedemo/feature1/Feature1OneFragment.kt
@@ -5,9 +5,9 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import kotlinx.android.synthetic.main.fragment_one.*
+import kotlinx.android.synthetic.main.fragment_feature1.*
 
-class Feature1OneFragment : Fragment(R.layout.fragment_one) {
+class Feature1OneFragment : Fragment(R.layout.fragment_feature1) {
 
     @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
--- feature1/src/main/java/com/flywith24/dynamicfeaturedemo/feature1/FragmentTwoFragment.kt
+++ feature1/src/main/java/com/flywith24/dynamicfeaturedemo/feature1/FragmentTwoFragment.kt
@@ -4,9 +4,9 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.fragment_one.*
+import kotlinx.android.synthetic.main.fragment_feature1.*
 
-class FragmentTwoFragment : Fragment(R.layout.fragment_one) {
+class FragmentTwoFragment : Fragment(R.layout.fragment_feature1) {
     @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         title.text = "Feature1Tow"
